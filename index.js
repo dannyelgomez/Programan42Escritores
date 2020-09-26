212eb68372cae41dd5e051b4580f3bce0f4bd151
Daniel Gomez <dannyelgomez@hotmail.com>
2020-09-26 14:17:06 -0500

    feat: Adding get and post method

--- index.js
+++ index.js
@@ -3,9 +3,65 @@ const app = express();
 app.use(express.json());
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Ok')
-});
+let autores = [];
+
+
+/* Escritores Genera un array y que cada posición contenga un elemento tipo objeto como el siguiente: */
+
+/* Rutas / Acciones
+/autores – 
+GET: devuelve todos los autores */
+
+app.get('/autores', (req, res) => {
+    res.send(autores)
+})
+
+/* POST: crea un nuevo autor  */
+
+app.post('/autores', (req, res) => {
+    const autor = {
+        id: autores.length + 1,
+        nombre: req.body.nombre,
+        apellido: req.body.apellido,
+        fechaDeNacimiento: req.body.fechaDeNacimiento,
+        libros: req.body.libros
+    };
+
+    autores.push(autor);
+    res.send('' + autor.id);
+})
+
+/* /autores/:id
+GET: devuelve el autor con el id indicado */
+
+
+/* DELETE: elimina el autor con el id indicado */
+
+/* PUT: modifica el autor con el id indicado */
+
+/* Valida a través de un middleware que el escritor exista en tu array */
+
+
+
+
+/* Rutas / Libros
+/autores/:id/libros
+GET: devuelve todos los libros de un autor */
+
+
+/* POST: agrega un nuevo libro al autor */
+
+
+/* Utiliza el mismo middleware para verificar que le autor exista */
+
+/* /autores/:id/libros/:idLibro
+GET: devuelve el libro con el id indicado del autor */
+
+/* PUT: modifica el libro con el id indicado del autor */
+
+/* DELETE: eliminar el libro con el id indicado del autor */
+
+/* Crea un nuevo middleware para verificar la existencia del libro y también que corresponda al autor */
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
